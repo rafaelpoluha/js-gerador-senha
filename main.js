b4e0a164eff1a394db2f7dcb61d8a84cbd7d315e
rafaelpoluha <163421927+rafaelpoluha@users.noreply.github.com>
2025-08-04 13:35:44 +0000

    selector

---
+++ main.js
@@ -0,0 +1,22 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclick = diminuirTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuirTamanho(){
+    if(tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha - 1;
+    }
+    numeroSenha.textcontent = tamanhoSenha;
+}
+
+function diminuirTamanho(){
+    if(tamanhoSenha > 20){
+        tamanhoSenha = tamanhoSenha + 1;
+    }
+    numeroSenha.textcontent = tamanhoSenha;
+}
